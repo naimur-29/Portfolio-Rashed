7f349feedaac78b80e6bfdb3bb949b75ff9009d3
Naim <skylike2904@gmail.com>
2022-03-28 15:38:40 +0600

    added about section

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,24 @@ const navbarMobileMenu = document.querySelector(
 );
 const navbarMenu = document.querySelector(".navbar__container .menu");
 
+const homeMenu = document.querySelector(".home__menu");
+const aboutMenu = document.querySelector(".about__menu");
+const skillsMenu = document.querySelector(".skills__menu");
+const servicesMenu = document.querySelector(".services__menu");
+const contactMenu = document.querySelector(".contact__menu");
+
+// TOGGLE MOBILE MENU
 navbarMobileMenu.addEventListener("click", () => {
   navbarMobileMenu.classList.toggle("mobile__menu__active");
   navbarMenu.classList.toggle("menu__active");
 });
+
+// TOGGLE MOBILE MENU ON CLICK ON MENUS
+[homeMenu, aboutMenu, skillsMenu, servicesMenu, contactMenu].forEach(
+  (element) => {
+    element.addEventListener("click", () => {
+      navbarMobileMenu.classList.toggle("mobile__menu__active");
+      navbarMenu.classList.toggle("menu__active");
+    });
+  }
+);
